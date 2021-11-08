23fbea0c138b405ca39d78a021d2b5fa913d645b
teodmir <kenneths@protonmail.com>
2021-11-08 18:44:01 +0100

    Customizable format

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 # mpv info daemon
   Daemon that outputs information about the currently playing file of mpv,
-  suitable for status bar output.
+  suitable for status bar output. This is a work in progress; while the output
+  works as intended and is free from bugs in my experience, there is no
+  customization and the output format is limited to [volume]% [mediatitle] |
+  [album].
+
+# Screenshot showing output in xmobar
+![xmobar output](screenshot.png?raw=true "Title")
+
+# Usage
+  - mpvinfod uses mpv's socket IPC feature to observe events from mpv. To enable this, mpv needs to be started
 
 # Dependencies
-  - inotify-simple
+  - inotify-simple: used to monitor the mpv socket for changes and detect when
+    the socket is started.
 
 # Todo
-  Custom format, command line flags (socket location, etc.), output formats
+  - JSON configuration with arbitrary mpv properties
+  - command line arguments
--- mpvinfod.py
+++ mpvinfod.py
@@ -5,49 +5,97 @@ import signal
 import sys
 import time
 import pathlib
+from string import Template
 from inotify_simple import INotify, flags
 
 RECV_CHUNK = 8192  # Size of byte chunks to read from the socket.
 CLIENT_ID = 1  # Client ID to be used in mpv communication.
 ADDR = '/tmp/mpvsocket'  # Location of the socket.
 EMPTYSTR = ""  # Placeholder text to use when not active
-MAXLEN = 100  # Maximum length of the output string
 
-# Make this non-global
-property_dict = {
-    'media-title': None,
-    'metadata/by-key/album': None,
-    'volume': None,
-    'loop-file': None
-}
+
+default_format = [
+    {
+        'property': 'volume',
+        'format': "($p%) ",
+        'max_length': 10,
+        'shorten_str': '...'
+    },
+    {
+        'property': 'media-title',
+        'format': "$p",
+        'max_length': 80,
+        'shorten_str': '...'
+    },
+    {
+        'property': 'metadata/by-key/album',
+        'format': " | $p",
+        'max_length': 50,
+        'shorten_str': '...'
+    }
+ ]
+
+
+# Cached values of properties of properties to their indices in
+# default_format.
+property_index_cache = {}
+
+
+# Formatted strings in the order of default_format. Empty strings are used for
+# values that don't exist yet.
+formatted_cache = []
+
+
+def format_cached():
+    "Concatenate formatted_cache."
+    return ''.join(formatted_cache)
+
+
+def wait():
+    time.sleep(0.1)
+
+
+def format_property(prop_name, prop_value):
+    spec = default_format[property_index_cache[prop_name]]
+    max_len = spec['max_length']
+    is_too_long = len(prop_value) > max_len
+    shortened = ((prop_value[:max_len] + spec['shorten_str'])
+                 if is_too_long else prop_value)
+    formatted = Template(spec['format']).substitute(p=shortened)
+    return formatted
 
 
 def signal_handler(sig, frame):
     """Empty the bar before exiting."""
-    def_empty()
+    output_empty()
     sys.exit(0)
 
 
 def observe(sock, prop):
     str = json.dumps({
-        'command': ['observe_property', CLIENT_ID, prop]
+        'command': ['observe_property_string', CLIENT_ID, prop]
     }) + '\n'
     sock.sendall(str.encode('UTF-8'))
 
 
 def get_jsons(str):
+    "Return list of jsons "
     return list(map(json.loads, str.splitlines()))
 
 
-def def_output(str):
+def output(str):
+    "Output the status bar message. Just print it."
     print(str, flush=True)
 
 
-def def_empty():
-    def_output(EMPTYSTR)
+def output_empty():
+    output(EMPTYSTR)
 
 
 def get_newest_data(json_list, event):
+    """Get the newest value for the provided event in the json list.
+Returns None if no new values exist.
+"""
     events = [
         j for j in json_list
         if list(map(j.get, ['event', 'id', 'name'])) ==
@@ -62,9 +110,12 @@ def get_newest_data(json_list, event):
 
 
 def wait_connect(inotify, sockname):
-    """Wait for the mpv server to start. This is done by using inotify to
+    """Wait for the mpv server to start.
+
+This is done by using inotify to
 passively listen for changes to the socket file and reconnecting
-whenever a change occurs."""
+whenever a change occurs.
+"""
     while True:
         try:
             sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
@@ -78,56 +129,30 @@ whenever a change occurs."""
                     if event.name == sockname:
                         found = True
             # Hack; sometimes connection refused otherwise (why?)
-            time.sleep(0.1)
-
-
-def format_properties():
-    """Convert the property dict to a string suitable for output."""
-    title = property_dict['media-title']
-    if not title:
-        return None
-    album = property_dict['metadata/by-key/album']
-    album_str = f' | {album}' if album else ''
-    volume = property_dict['volume']
-    volume_str = f'({str(int(volume))}%)' if volume else ''
-    repeat = property_dict['loop-file']
-    repeat_str = ' (r) ' if repeat else ' '
-    full = f'{volume_str}{repeat_str}{title}{album_str}'
-    output = full if len(full) <= MAXLEN else full[0:MAXLEN] + "..."
-    return output
-
-
-def empty_or_default(s, default):
-    """Use 'or' without using the empty string as a false value."""
-    return '' if s == '' else s or default
-
-
-def new_dict(json_list):
-    """Update the property dict (if newer values are available)."""
-    return {
-        k: empty_or_default(get_newest_data(json_list, k), v)
-        for k, v in property_dict.items()
-    }
+            wait()
 
 
 def request_observers(sock):
-    """Send observe requests to mpv."""
-    for event in property_dict.keys():
-        observe(sock, event)
+    """Send observe requests to mpv based on properties."""
+    for spec in default_format:
+        observe(sock, spec['property'])
 
 
 def end_session(sock):
-    def_empty()
+    output_empty()
     # For some reason, connect() will reuse the old socket unless we wait a bit
     # (TIME_WAIT state?), resulting in a broken pipe error when sending the
     # data. This hack ensures that a fresh socket is used.
-    time.sleep(0.1)
+    wait()
 
 
 def run_observer(sock):
-    """Main program loop."""
-    global property_dict
-    reset_dict()
+    """Main program loop.
+
+Read from the mpv socket until the connection is closed.
+"""
+    # Ensure empty cache when starting.
+    reset_format_cache()
     while True:
         try:
             contents = sock.recv(RECV_CHUNK)
@@ -138,23 +163,31 @@ def run_observer(sock):
             end_session(sock)
             return
         json_list = get_jsons(contents.decode('UTF-8', 'ignore'))
+        for spec in default_format:
+            prop = spec['property']
+            value = get_newest_data(json_list, prop)
+            if value:
+                formatted_cache[property_index_cache[prop]] = format_property(prop, value)
+        output(format_cached())
         # Update the dictionary and output only when necessary: that is, only
         # when any of the observed properties have new values.
-        new = new_dict(json_list)
-        if new != property_dict:
-            property_dict = new
-            def_output(format_properties())
 
 
-def reset_dict():
-    """Set all property dict values to None."""
-    for k in property_dict.keys():
-        property_dict[k] = None
+def generate_prop_index():
+    global default_format
+    for idx, spec in enumerate(default_format):
+        property_index_cache[spec['property']] = idx
+
+
+def reset_format_cache():
+    global formatted_cache
+    formatted_cache = [''] * len(default_format)
 
 
 def run():
+    generate_prop_index()
     signal.signal(signal.SIGINT, signal_handler)
-    def_empty()
+    output_empty()
     inotify = INotify()
     watch_flags = flags.CREATE
     watch_dir = pathlib.Path(ADDR).parent
