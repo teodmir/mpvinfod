9521afde7977d2ba86d6b55897a3d1adec33677f
teodmir <kenneths@protonmail.com>
2021-12-30 12:51:14 +0100

    Change configuration format and add proper readme

--- README.md
+++ README.md
@@ -1,20 +1,63 @@
 # mpv info daemon
-  Daemon that outputs information about the currently playing file of mpv,
-  suitable for status bar output. This is a work in progress; while the output
-  works as intended and is free from bugs in my experience, there is no
-  customization and the output format is limited to [volume]% [mediatitle] |
-  [album].
+Daemon that outputs information about the currently playing file of mpv,
+suitable for status bar output. This is a work in progress, but it should be
+bug free as long as your configuration doesn't contain errors.
 
 # Screenshot showing output in xmobar
 ![xmobar output](screenshot.png?raw=true "Title")
 
 # Usage
-  - mpvinfod uses mpv's socket IPC feature to observe events from mpv. To enable this, mpv needs to be started
+  mpvinfod uses mpv's socket IPC feature to observe events from mpv. To enable
+  this, mpv needs to be started with the `--input-ipc-server` option (see the
+  mpv manual for details). Since it only makes sense to output information from
+  a single instance of mpv (what would the output look like from multiple mpv
+  instances?), you should either use a single instance of mpv (see the umpv
+  script that is provided with mpv) or use a shell alias if you only want this
+  feature enabled at certain times. I personally use mpv to play music this way
+  and use a shell alias that disables video playback using `--vo=null` along
+  with the IPC server location.
 
 # Dependencies
   - inotify-simple: used to monitor the mpv socket for changes and detect when
     the socket is started.
 
-# Todo
-  - JSON configuration with arbitrary mpv properties
-  - command line arguments
+# Configuration
+mpvinfod uses a JSON configuration file located at
+`${XDG_CONFIG_HOME}/mpvinfod/config.json` or `~/.config/mpvinfod/config` if
+`XDG_CONFIG_HOME` is undefined. An example configuration is provided in
+examples/config.json. The configuration itself consists of two parts: format,
+which is a format string where mpv properties written as {property} are
+replaced by the value of the corresponding mpv property. For a list of
+available properties, see the "Property list" section of the mpv manual. As the
+manual itself states, some options are also available as properties, such as
+"mute".
+
+The second part is the optional custom part, which defines how the property
+should be display. Available properties are:
+
+- empty: String to use when no video is available. Defaults to the empty string
+  (empty output in the status bar)
+
+- socket: The path to the socket. Defaults to /mpvsocket.
+
+- client_id: The request identifier that should uniquely identify this
+  application. If you aren't using anything else that uses the JSON IPC feature
+  of mpv then you shouldn't have to change this. Defaults to 1.
+
+- custom: "Property display customization" below.
+
+## Property display customization
+- format: How the string should be formatted; a format string inside the format
+  string, basically. The placeholder text `{prop}` is replaced with the actual
+  value. Useful for adding extra strings only when the property is present (see
+  the volume and album customizations in the example configuration for ideas).
+  Default: "{prop}"
+
+- max_length: if the resulting string length is longer than this, then the
+  string will be shortened. Default: 50 characters.
+
+- shorten_str: String that is appended if the string was shortened.
+  Default: "...".
+
+- replace: An object that maps strings to strings; the key being a value to
+  match, and the mapping being the value to replace it with.
--- mpvinfod.py
+++ mpvinfod.py
@@ -7,64 +7,27 @@ import time
 import pathlib
 import os
 import os.path
-from string import Template
+from string import Formatter
 from inotify_simple import INotify, flags
 
 RECV_CHUNK = 32768  # Max size of bytes to read from the socket.
-CLIENT_ID = 1  # Client ID to be used in mpv communication.
-ADDR = '/tmp/mpvsocket'  # Location of the socket.
-EMPTYSTR = ''  # Placeholder text to use when not active
 
-
-default_config = [
-    {
-        'property': 'loop-file',
-        'format': '$p ',
-        'replace_map': {
-            'no': '',
-            'inf': '(r)'
-        },
-        'max_length': 5
-    },
-    {
-        'property': 'volume',
-        'format': "($p%) ",
-        'type': 'int',
-        'max_length': 5,
-    },
-    {
-        'property': 'media-title',
-        'format': '$p',
-        'max_length': 80,
-    },
-    {
-        'property': 'metadata/by-key/album',
-        'format': ' | $p',
-        'max_length': 50,
-    }
- ]
-
-
-default_spec_values = {
-    'type': 'string',
-    'format': '$p',
-    'max_length': 50,
-    'shorten_str': '...',
-    'replace_map': {}
+default_config = {
+    "format": "mpv: {media-title}",
+    "empty": "",
+    "socket": "/tmp/mpvsocket",
+    "client_id": 1
 }
 
 
-user_config = default_config
-
-
-# Formatted strings in the order of default_config. Empty strings are used for
-# values that don't exist yet.
-# formatted_cache = []
-
-
-# def format_cached():
-#     "Concatenate formatted_cache."
-#     return ''.join(formatted_cache)
+# Default value to use for property customizations.
+default_custom_values = {
+    'format': '{prop}',
+    'max_length': 50,
+    'shorten_str': '...',
+    'replace': {},
+    'as_int': False
+}
 
 
 def wait():
@@ -72,38 +35,36 @@ def wait():
     time.sleep(0.1)
 
 
-def format_property(prop_spec, prop_value):
-    "Form prop_value according to its specification."
-    # prop_spec = user_config[property_index_cache[prop_name]]
-    if prop_spec['type'] == 'int':
+def format_property(prop_custom, prop_value):
+    "Form prop_value according to its customification."
+    if prop_custom['as_int']:
         prop_value = str(int(prop_value))
 
-    replace_val = prop_spec['replace_map'].get(prop_value)
+    replace_val = prop_custom['replace'].get(prop_value)
     if replace_val is not None:
         prop_value = replace_val
 
-    max_len = prop_spec['max_length']
+    # Empty replacement strings are treated specially; treated as if they were
+    # empty to begin with.
+    if replace_val == '':
+        return ''
+
+    max_len = prop_custom['max_length']
     is_too_long = len(prop_value) > max_len
-    shortened = ((prop_value[:max_len] + prop_spec['shorten_str'])
+    shortened = ((prop_value[:max_len] + prop_custom['shorten_str'])
                  if is_too_long else prop_value)
-    formatted = Template(prop_spec['format']).substitute(p=shortened)
+    formatted = prop_custom['format'].format(prop=shortened)
     return formatted
 
 
-def signal_handler(sig, frame):
-    """Empty the bar before exiting."""
-    output_empty()
-    sys.exit(0)
-
-
-def observe(sock, prop, str=True):
+def observe(sock, prop, client_id, native=False):
     """Observe the given property. By default all properties will be strings,
-    but with the named argument str set to False, return the property in its
-    native form.
+    but with the named argument 'native' set to True, return the property in
+    its native form.
     """
-    cmd = 'observe_property_string' if str else 'observe_property'
+    cmd = 'observe_property' if native else 'observe_property_string'
     str = json.dumps({
-        'command': [cmd, CLIENT_ID, prop]
+        'command': [cmd, client_id, prop]
     }) + '\n'
     sock.sendall(str.encode('UTF-8'))
 
@@ -118,19 +79,14 @@ def output(str):
     print(str, flush=True)
 
 
-def output_empty():
-    "Output placeholder string."
-    output(EMPTYSTR)
-
-
-def get_newest_data(json_list, event):
+def get_newest_data(json_list, event, client_id):
     """Get the newest value for the provided event in the json list.
 Returns None if no new values exist.
 """
     events = [
         j for j in json_list
         if list(map(j.get, ['event', 'id', 'name'])) ==
-        ['property-change', CLIENT_ID, event]
+        ['property-change', client_id, event]
     ]
     if not events:  # No events at all
         return None
@@ -138,10 +94,10 @@ Returns None if no new values exist.
         data = events[-1].get('data')
         # Use empty string to differentiate between 'no events found' and
         # 'newest data is null'.
-        return data or ''
+        return data if data is not None else ''
 
 
-def wait_connect(inotify, sockname):
+def wait_connect(inotify, sockname, addr):
     """Wait for the mpv server to start and return the socket.
 
 This is done by using inotify to passively listen for changes to the socket
@@ -151,7 +107,7 @@ well, but this has performance advantages.
     while True:
         try:
             sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-            sock.connect(ADDR)
+            sock.connect(addr)
             return sock
         except ConnectionError:
             # Socket unavailable; wait for changes to the address.
@@ -166,62 +122,52 @@ well, but this has performance advantages.
 
 def request_observers(sock, config):
     "Send observe requests to mpv based on the properties given."
-    for spec in config:
-        as_str = spec['type'] == 'string'
-        observe(sock, spec['property'], as_str)
+    props = get_requested_properties(config)
+    for p in props:
+        as_int = config['custom'][p]['as_int']
+        observe(sock, p, config['client_id'], as_int)
 
 
-def end_session(sock):
-    output_empty()
+def end_session(end_str):
+    output(end_str)
     # For some reason, connect() will reuse the old socket unless we wait a bit
     # (TIME_WAIT state?), resulting in a broken pipe error when sending the
     # data. This hack ensures that a fresh socket is used.
     wait()
 
 
-def run_observer(sock, prop_index):
+def run_observer(sock, config):
     """Main program loop.
 
 Read from the mpv socket until the connection is closed.
 Needs a prop_index that maps property names to indices in
 """
-    # Ensure empty cache when starting.
-    formatted_cache = []
+    props = get_requested_properties(config)
+    # A map of property names to their formatted values.
+    property_cache = {p: '' for p in props}
     while True:
         try:
             contents = sock.recv(RECV_CHUNK)
         except ConnectionResetError:
-            end_session(sock)
+            end_session(config['empty'])
             return
         if not contents:  # Connection closed.
-            end_session(sock)
+            end_session(config['empty'])
             return
         # Make sure no errors are raised when decoding due to strange
         # encodings.
         json_list = get_jsons(contents.decode('UTF-8', 'ignore'))
-        for spec in user_config:
-            prop = spec['property']
-            value = get_newest_data(json_list, prop)
+        for prop in props:
+            value = get_newest_data(json_list, prop, config['client_id'])
             if value == '':
-                # If the string is empty the property is no longer available,
-                # so it shouldn't be formatted.
-                formatted_cache[prop_index[prop]] = ''
+                # If the string is empty the property is no longer available
+                # (an example being a new song having no album title), so it
+                # shouldn't be formatted, since it might show the old name.
+                property_cache[prop] = ''
             elif value is not None:
-                formatted_cache[prop_index[prop]] = format_property(
-                    prop, value)
-        output(format_cached())
-
-
-def generate_prop_index(config):
-    "Map the properties in the user_config to indices"
-    return {spec['property']: idx for idx, spec in enumerate(config)}
-    # for idx, spec in enumerate(config):
-    #     property_index_cache[spec['property']] = idx
-
-
-def reset_format_cache():
-    global formatted_cache
-    formatted_cache = [''] * len(user_config)
+                property_cache[prop] = format_property(config['custom'][prop],
+                                                       value)
+        output(config['format'].format(**property_cache))
 
 
 def fix_config(config):
@@ -231,10 +177,23 @@ Involves adding default values, ensuring properties are only used once, etc.
 Returns the newly created configuration. Throws an exception if there are
 errors.
 """
-    merged_config = [{}] * len(config)
-    for idx, spec in enumerate(config):
-        merged_config[idx] = {**default_spec_values, **spec}
-    return merged_config
+    props = get_requested_properties(config)
+
+    # Use default values for all configuration options except custom, which has
+    # its own default format.
+    for key, val in default_config.items():
+        if key != 'custom' and key != 'format':
+            config[key] = val
+
+    # Create custom section if it doesn't exist and fill it with default
+    # values for each property in the format string. .
+    if 'custom' not in config:
+        config['custom'] = {}
+    for p in props:
+        #
+        config['custom'][p] = {**default_custom_values,
+                               **(config['custom'].get(p) or {})}
+    return config
 
 
 def find_config_file():
@@ -248,31 +207,45 @@ $XDG_CONFIG_HOME/mpvinfod/config.json or ~/.config/mpvinfod/config.json
     return config_file if os.path.isfile(config_file) else None
 
 
+def get_requested_properties(config):
+    """Find the properties listed in the format string."""
+    return [
+        fn for _, fn, _, _ in
+        Formatter().parse(config['format']) if fn is not None
+    ]
+
+
 def run():
     "Set up the program loop and run it."
-    global user_config
     config_file = find_config_file()
     if config_file:
         with open(config_file) as f:
-            user_config = json.load(f)
-    user_config = fix_config(user_config)
+            user_config = fix_config(json.load(f))
+    else:
+        user_config = fix_config(default_config)
 
-    prop_index = generate_prop_index(user_config)
+    def signal_handler(sig, frame):
+        """Empty the bar before exiting."""
+        output(user_config['empty'])
+        sys.exit(0)
 
     signal.signal(signal.SIGINT, signal_handler)
-    output_empty()
+    # Output the placeholder string to begin with.
+    output(user_config['empty'])
+
+    addr = user_config['socket']
 
     inotify = INotify()
     watch_flags = flags.CREATE
-    watch_dir = pathlib.Path(ADDR).parent
-    watch_file = pathlib.Path(ADDR).stem
+    watch_dir = pathlib.Path(addr).parent
+    watch_file = pathlib.Path(addr).stem
     inotify.add_watch(watch_dir, watch_flags)
     while True:
         # Using garbage collection to close the socket instead of an explicit
         # close() call.
-        with wait_connect(inotify, watch_file) as sock:
+        with wait_connect(inotify, watch_file, addr) as sock:
             request_observers(sock, user_config)
-            run_observer(sock, prop_index)
+            run_observer(sock, user_config)
 
 
 if __name__ == "__main__":
